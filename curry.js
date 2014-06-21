c7b8fda56e2cc06dca9596b4a74b7fc967406aeb
YiChenshu <ycswaves@gmail.com>
2014-06-21 22:36:39 +0800

    complete

--- curry.js
+++ curry.js
@@ -13,3 +13,27 @@ function curryN(fn, n) {
 }
 
 module.exports = curryN
+
+
+// Tim's answer
+function curryN_Tim(fn, n) {
+  // If `n` argument was omitted, use the function .length property.
+  if (typeof n !== 'number') n = fn.length
+
+  function getCurriedFn(prev) {
+    return function(arg) {
+      // Concat the just-specified argument with the array of
+      // previously-specified arguments.
+      var args = prev.concat(arg)
+      // Not all arguments have been satisfied yet, so return a curried
+      // version of the original function.
+      if (args.length < n) return getCurriedFn(args)
+      // Otherwise, invoke the original function with the arguments and
+      // return its value.
+      else return fn.apply(this, args)
+    };
+  }
+
+  // Return a curried version of the original function.
+  return getCurriedFn([])
+}
---
+++ fnCall.js
@@ -0,0 +1,18 @@
+module.exports = function(arr, begin, len){
+  return Array.prototype.slice.call(arr, begin, len);
+}
+
+// Tim's answer
+// Explained:
+// The value of `this` in Function.call is the function
+// that will be executed.
+//
+// Bind returns a new function with the value of `this` fixed
+// to whatever was passed as its first argument.
+//
+// Every function 'inherits' from Function.prototype,
+// thus every function, including call, apply and bind
+// have the methods call apply and bind.
+//
+// Function.prototype.call === Function.call
+// module.exports = Function.call.bind(Array.prototype.slice)
